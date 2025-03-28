a6f065ea7cd42a7ad2f7b4181ad29e59df9b5603
kamil jk <kamil.j.krawczyk@gmail.com>
2025-03-28 14:49:56 -0500

    Add new pages for library and liminology, and update dependencies in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import content from "@astrojs/content"; // <-- add this
 
 export default defineConfig({
 	site: "https://astro-nano-demo.vercel.app",
-	integrations: [mdx(), sitemap(), tailwind(), content()], // <-- add here
+	integrations: [mdx(), sitemap(), tailwind()], // <-- add here
 });
